7c390f98154cc9b1ea6b6ee3247398f7cb194800
krasi <krasimir.xristov@gmail.com>
2024-04-14 23:45:39 +0200

    Change the responsive header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -89,12 +89,12 @@ const Header = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden md:hidden' color='gray' pill>
+      <Button className='w-12 h-10 hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
         <Button
-          className='w-12 h-10 hidden sm:inline'
+          className='w-12 h-10'
           color='gray'
           pill
           onClick={() => dispatch(toggleTheme())}
