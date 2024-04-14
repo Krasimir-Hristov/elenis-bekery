c145b7d874651d43af7748d82ec5c64e6dcd4461
krasi <krasimir.xristov@gmail.com>
2024-04-14 21:03:46 +0200

    change the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,39 @@
-import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  Navbar,
+  NavbarCollapse,
+  TextInput,
+} from 'flowbite-react';
+
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { FaMoon } from 'react-icons/fa';
+import { FaSun } from 'react-icons/fa';
+
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useEffect, useState } from 'react';
 
-export default function Header() {
+const Header = () => {
   const path = useLocation().pathname;
   const location = useLocation();
-  const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get('searchTerm');
+
     if (searchTermFromUrl) {
       setSearchTerm(searchTermFromUrl);
     }
@@ -29,7 +44,7 @@ export default function Header() {
       const res = await fetch('/api/user/signout', {
         method: 'POST',
       });
-      const data = await res.json();
+
       if (!res.ok) {
         console.log(data.message);
       } else {
@@ -42,34 +57,39 @@ export default function Header() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const urlParams = new URLSearchParams(location.search);
     urlParams.set('searchTerm', searchTerm);
     const searchQuery = urlParams.toString();
+
     navigate(`/search?${searchQuery}`);
   };
-
   return (
     <Navbar className='border-b-2'>
       <Link
         to='/'
-        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+        className='flex items-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-          Sahand's
-        </span>
-        Blog
+        <div className='w-16 h-16'>
+          <img
+            className='w-full h-full p-2'
+            src='/public/logo.png'
+            alt='logo'
+          />
+        </div>
       </Link>
+
       <form onSubmit={handleSubmit}>
         <TextInput
           type='text'
-          placeholder='Search...'
+          placeholder='Suchen...'
           rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
+          className='lg:inline md:inline'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+      <Button className='w-12 h-10 lg:hidden md:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
@@ -86,7 +106,7 @@ export default function Header() {
             arrowIcon={false}
             inline
             label={
-              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+              <Avatar alt='user' img={currentUser?.profilePicture} rounded />
             }
           >
             <Dropdown.Header>
@@ -96,31 +116,31 @@ export default function Header() {
               </span>
             </Dropdown.Header>
             <Link to={'/dashboard?tab=profile'}>
-              <Dropdown.Item>Profile</Dropdown.Item>
+              <Dropdown.Item>Profil</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+            <Dropdown.Item onClick={handleSignout}>Abmelden</Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to='/sign-in'>
-            <Button gradientDuoTone='purpleToBlue' outline>
-              Sign In
+            <Button gradientDuoTone='greenToBlue' outline>
+              Anmelden
             </Button>
           </Link>
         )}
         <Navbar.Toggle />
       </div>
-      <Navbar.Collapse>
+      <NavbarCollapse>
         <Navbar.Link active={path === '/'} as={'div'}>
-          <Link to='/'>Home</Link>
+          <Link to='/'>Startseite</Link>
         </Navbar.Link>
         <Navbar.Link active={path === '/about'} as={'div'}>
-          <Link to='/about'>About</Link>
+          <Link to='/about'>Über uns</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as={'div'}>
-          <Link to='/projects'>Projects</Link>
-        </Navbar.Link>
-      </Navbar.Collapse>
+
+      </NavbarCollapse>
     </Navbar>
   );
-}
+};
+
+export default Header;
