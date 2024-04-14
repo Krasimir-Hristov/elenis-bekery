3625f87a1626455bde185f3bfdcf4dfdca0c51ad
krasi <krasimir.xristov@gmail.com>
2024-04-14 23:03:07 +0200

    change search

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,13 +1,13 @@
-import { Button, Select, TextInput } from 'flowbite-react';
+// import { Button, Select, TextInput } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import PostCard from '../components/PostCard';
 
 export default function Search() {
   const [sidebarData, setSidebarData] = useState({
     searchTerm: '',
     sort: 'desc',
-    category: 'uncategorized',
+    category: 'unkategorisiert',
   });
 
   console.log(sidebarData);
@@ -17,7 +17,7 @@ export default function Search() {
 
   const location = useLocation();
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -55,29 +55,29 @@ export default function Search() {
     fetchPosts();
   }, [location.search]);
 
-  const handleChange = (e) => {
-    if (e.target.id === 'searchTerm') {
-      setSidebarData({ ...sidebarData, searchTerm: e.target.value });
-    }
-    if (e.target.id === 'sort') {
-      const order = e.target.value || 'desc';
-      setSidebarData({ ...sidebarData, sort: order });
-    }
-    if (e.target.id === 'category') {
-      const category = e.target.value || 'uncategorized';
-      setSidebarData({ ...sidebarData, category });
-    }
-  };
+  // const handleChange = (e) => {
+  //   if (e.target.id === 'searchTerm') {
+  //     setSidebarData({ ...sidebarData, searchTerm: e.target.value });
+  //   }
+  //   if (e.target.id === 'sort') {
+  //     const order = e.target.value || 'desc';
+  //     setSidebarData({ ...sidebarData, sort: order });
+  //   }
+  //   if (e.target.id === 'category') {
+  //     const category = e.target.value || 'unkategorisiert';
+  //     setSidebarData({ ...sidebarData, category });
+  //   }
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const urlParams = new URLSearchParams(location.search);
-    urlParams.set('searchTerm', sidebarData.searchTerm);
-    urlParams.set('sort', sidebarData.sort);
-    urlParams.set('category', sidebarData.category);
-    const searchQuery = urlParams.toString();
-    navigate(`/search?${searchQuery}`);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const urlParams = new URLSearchParams(location.search);
+  //   urlParams.set('searchTerm', sidebarData.searchTerm);
+  //   urlParams.set('sort', sidebarData.sort);
+  //   urlParams.set('category', sidebarData.category);
+  //   const searchQuery = urlParams.toString();
+  //   navigate(`/search?${searchQuery}`);
+  // };
 
   const handleShowMore = async () => {
     const numberOfPosts = posts.length;
@@ -101,55 +101,14 @@ export default function Search() {
   };
 
   return (
-    <div className='flex flex-col md:flex-row'>
-      <div className='p-7 border-b md:border-r md:min-h-screen border-gray-500'>
-        <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
-          <div className='flex   items-center gap-2'>
-            <label className='whitespace-nowrap font-semibold'>
-              Search Term:
-            </label>
-            <TextInput
-              placeholder='Search...'
-              id='searchTerm'
-              type='text'
-              value={sidebarData.searchTerm}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='flex items-center gap-2'>
-            <label className='font-semibold'>Sort:</label>
-            <Select onChange={handleChange} value={sidebarData.sort} id='sort'>
-              <option value='desc'>Latest</option>
-              <option value='asc'>Oldest</option>
-            </Select>
-          </div>
-          <div className='flex items-center gap-2'>
-            <label className='font-semibold'>Category:</label>
-            <Select
-              onChange={handleChange}
-              value={sidebarData.category}
-              id='category'
-            >
-              <option value='uncategorized'>Uncategorized</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
-              <option value='javascript'>JavaScript</option>
-            </Select>
-          </div>
-          <Button type='submit' outline gradientDuoTone='purpleToPink'>
-            Apply Filters
-          </Button>
-        </form>
-      </div>
-      <div className='w-full'>
+    <div className='flex flex-col md:flex-row justify-center items-center'>
+      {/* ... */}
+      <div className='w-full text-center'>
         <h1 className='text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5 '>
-          Posts results:
+          Beitragsergebnisse:
         </h1>
-        <div className='p-7 flex flex-wrap gap-4'>
-          {!loading && posts.length === 0 && (
-            <p className='text-xl text-gray-500'>No posts found.</p>
-          )}
-          {loading && <p className='text-xl text-gray-500'>Loading...</p>}
+        <div className='p-7 flex flex-wrap gap-4 justify-center'>
+          {/* ... */}
           {!loading &&
             posts &&
             posts.map((post) => <PostCard key={post._id} post={post} />)}
@@ -158,11 +117,82 @@ export default function Search() {
               onClick={handleShowMore}
               className='text-teal-500 text-lg hover:underline p-7 w-full'
             >
-              Show More
+              Mehr anzeigen
             </button>
           )}
         </div>
       </div>
     </div>
+    // <div className='flex flex-col md:flex-row'>
+    //   <div className='p-7 border-b md:border-r md:min-h-screen border-gray-500'>
+    //     <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
+    //       <div className='flex   items-center gap-2'>
+    //         <label className='whitespace-nowrap font-semibold'>
+    //           Suchbegriff:
+    //         </label>
+    //         <TextInput
+    //           placeholder='Suchen...'
+    //           id='searchTerm'
+    //           type='text'
+    //           value={sidebarData.searchTerm}
+    //           onChange={handleChange}
+    //         />
+    //       </div>
+    //       <div className='flex items-center gap-2'>
+    //         <label className='font-semibold'>Sort:</label>
+    //         <Select onChange={handleChange} value={sidebarData.sort} id='sort'>
+    //           <option value='desc'>Neueste</option>
+    //           <option value='asc'>Älteste</option>
+    //         </Select>
+    //       </div>
+    //       <div className='flex items-center gap-2'>
+    //         <label className='font-semibold'>Kategorie:</label>
+    //         <Select
+    //           onChange={handleChange}
+    //           value={sidebarData.category}
+    //           id='category'
+    //         >
+    //           <option value='unkategorisiert'>Unkategorisiert</option>
+    //           <option value='pizza'>Pizza</option>
+    //           <option value='hotdog'>Hot Dog</option>
+    //           <option value='bougatsa'>Bougatsa</option>
+    //           <option value='backwaren'>Backwaren</option>
+    //           <option value='brote'>Brote</option>
+    //           <option value='Kuchen'>Kuchen</option>
+    //           <option value='torten'>Torten</option>
+    //           <option value='kaffee'>Kaffee</option>
+    //           <option value='griechischespezialitäten'>
+    //             Griechische Spezialitäten
+    //           </option>
+    //         </Select>
+    //       </div>
+    //       <Button type='submit' outline>
+    //         Apply Filters
+    //       </Button>
+    //     </form>
+    //   </div>
+    //   <div className='w-full'>
+    //     <h1 className='text-3xl font-semibold sm:border-b  border-gray-500 p-3 mt-5 '>
+    //       Beitragsergebnisse:
+    //     </h1>
+    //     <div className='p-7 flex flex-wrap gap-4'>
+    //       {!loading && posts.length === 0 && (
+    //         <p className='text-xl text-gray-500'>Keine Beiträge gefunden.</p>
+    //       )}
+    //       {loading && <p className='text-xl text-gray-500'>Laden...</p>}
+    //       {!loading &&
+    //         posts &&
+    //         posts.map((post) => <PostCard key={post._id} post={post} />)}
+    //       {showMore && (
+    //         <button
+    //           onClick={handleShowMore}
+    //           className='text-teal-500 text-lg hover:underline p-7 w-full'
+    //         >
+    //           Mehr anzeigen
+    //         </button>
+    //       )}
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
