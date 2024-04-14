216c9d7fb38d87341b7568e56fa49f4d58b0326b
krasi <krasimir.xristov@gmail.com>
2024-04-14 23:15:28 +0200

    Change the signIn and sifnUp pages

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,36 +1,46 @@
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
+
 import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+
 import { useDispatch, useSelector } from 'react-redux';
 import {
   signInStart,
   signInSuccess,
   signInFailure,
 } from '../redux/user/userSlice';
+
 import OAuth from '../components/OAuth';
 
-export default function SignIn() {
+const SignIn = () => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({});
+
   const { loading, error: errorMessage } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (!formData.email || !formData.password) {
-      return dispatch(signInFailure('Please fill all the fields'));
+      return dispatch(signInFailure('All fields are required!'));
     }
     try {
       dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {
+          'Content-Type': 'application/json',
+        },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if (data.success === false) {
+
+      if (!res.ok) {
         dispatch(signInFailure(data.message));
       }
 
@@ -45,61 +55,60 @@ export default function SignIn() {
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
-        {/* left */}
         <div className='flex-1'>
-          <Link to='/' className='font-bold dark:text-white text-4xl'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Sahand's
-            </span>
-            Blog
+          <Link
+            to='/'
+            className='flex  items-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+          >
+            <div className='w-13 h-13 flex'>
+              <img className='w-full h-full p-2' src='/logo.png' alt='logo' />
+            </div>
           </Link>
           <p className='text-sm mt-5'>
-            This is a demo project. You can sign in with your email and password
-            or with Google.
+            Willkommen! Bitte melden Sie sich an, um Ihre Meinung zu unseren
+            Produkten zu teilen.
           </p>
         </div>
-        {/* right */}
-
         <div className='flex-1'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value='Your email' />
+              <Label value='Deine E-Mail' />
               <TextInput
                 type='email'
-                placeholder='name@company.com'
+                placeholder='Name@company.com'
                 id='email'
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value='Your password' />
+              <Label value='Deine passwort' />
               <TextInput
                 type='password'
-                placeholder='**********'
+                placeholder='**************'
                 id='password'
                 onChange={handleChange}
               />
             </div>
             <Button
-              gradientDuoTone='purpleToPink'
+              gradientDuoTone='greenToBlue'
               type='submit'
               disabled={loading}
             >
               {loading ? (
-                <>
-                  <Spinner size='sm' />
-                  <span className='pl-3'>Loading...</span>
-                </>
+                <div>
+                  <Spinner size='sm' /> <span className='pl-3'>Laden...</span>{' '}
+                </div>
               ) : (
-                'Sign In'
+                'Anmelden'
               )}
             </Button>
+
             <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Dont Have an account?</span>
-            <Link to='/sign-up' className='text-blue-500'>
-              Sign Up
+            <span>Sie haben kein Konto?</span>
+            <Link to='/sign-up' className='text-red-600'>
+              Hier anmelden
             </Link>
           </div>
           {errorMessage && (
@@ -111,4 +120,6 @@ export default function SignIn() {
       </div>
     </div>
   );
-}
+};
+
+export default SignIn;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,34 +3,42 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
 
-export default function SignUp() {
-  const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+const SignUp = () => {
   const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (!formData.username || !formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+      return setErrorMessage('Please fill out all fields');
     }
     try {
       setLoading(true);
-      setErrorMessage(null);
+      setErrorMessage('');
       const res = await fetch('/api/auth/signup', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {
+          'Content-Type': 'application/json',
+        },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if (data.success === false) {
+
+      if (!res.ok) {
+        setLoading(false);
         return setErrorMessage(data.message);
       }
-      setLoading(false);
-      if(res.ok) {
-        navigate('/sign-in');
+
+      if (res.ok) {
+        setLoading(false);
+        navigate('/');
       }
     } catch (error) {
       setErrorMessage(error.message);
@@ -40,70 +48,73 @@ export default function SignUp() {
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
-        {/* left */}
         <div className='flex-1'>
-          <Link to='/' className='font-bold dark:text-white text-4xl'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Sahand's
-            </span>
-            Blog
+          <Link
+            to='/'
+            className='flex  items-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+          >
+            <div className='w-13 h-13 flex'>
+              <img
+                className='w-full h-full p-2'
+                src='/public/logo.png'
+                alt='logo'
+              />
+            </div>
           </Link>
           <p className='text-sm mt-5'>
-            This is a demo project. You can sign up with your email and password
-            or with Google.
+            Willkommen! Bitte registrieren Sie sich, um Ihre Meinung zu unseren
+            Produkten zu teilen.
           </p>
         </div>
-        {/* right */}
-
         <div className='flex-1'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value='Your username' />
+              <Label value='Ihr Benutzername' />
               <TextInput
                 type='text'
-                placeholder='Username'
+                placeholder='Benutzername'
                 id='username'
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value='Your email' />
+              <Label value='Deine E-Mail' />
               <TextInput
                 type='email'
-                placeholder='name@company.com'
+                placeholder='Name@company.com'
                 id='email'
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value='Your password' />
+              <Label value='Deine passwort' />
               <TextInput
                 type='password'
-                placeholder='Password'
+                placeholder='Passwort'
                 id='password'
                 onChange={handleChange}
               />
             </div>
             <Button
-              gradientDuoTone='purpleToPink'
+              gradientDuoTone='greenToBlue'
               type='submit'
               disabled={loading}
             >
               {loading ? (
-                <>
-                  <Spinner size='sm' />
-                  <span className='pl-3'>Loading...</span>
-                </>
+                <div>
+                  <Spinner size='sm' /> <span className='pl-3'>Laden...</span>{' '}
+                </div>
               ) : (
-                'Sign Up'
+                'Anmelden'
               )}
             </Button>
+
             <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Have an account?</span>
-            <Link to='/sign-in' className='text-blue-500'>
-              Sign In
+            <span>Hast du bereits ein Konto?</span>
+            <Link to='/sign-in' className='text-red-600'>
+              Hier anmelden.
             </Link>
           </div>
           {errorMessage && (
@@ -115,4 +126,6 @@ export default function SignUp() {
       </div>
     </div>
   );
-}
+};
+
+export default SignUp;
